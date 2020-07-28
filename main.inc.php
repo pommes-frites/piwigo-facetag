1c6e057edb5766f2a9d38894bac2e57cfebeb2d0
Sven-Erik <sven.erik.andersen@gmail.com>
2020-07-28 16:31:53 +0200

    Update main.inc.php
    From [array_intersect issue #3](https://github.com/cccraig/MugShot/pull/7/commits/f12b1a40a4e0f20968dcbfb20b39e28217e7a34c)

--- main.inc.php
+++ main.inc.php
@@ -42,7 +42,10 @@ $current_user_groups = query_mugshot_groups();
 if ($current_user_groups != 0) {
   $plugin_config = unserialize(conf_get_param(MUGSHOT_ID));
   $group_list = $plugin_config['groups'];
-  $intersect = array_intersect((array)$group_list, $current_user_groups);
+  $intersect = [];
+  if(is_array($group_list)) {
+    $intersect = array_intersect($group_list, $current_user_groups);
+  }
 }
 
 if (is_array($current_user_groups) && count($intersect) != 0) {
