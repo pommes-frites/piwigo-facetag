3bce78d39adfc8e2b92b40482832cde21abfa4bc
Collin Craige <collincraige@gmail.com>
2022-02-12 12:26:21 -0600

    disable autocomplete for name text boxes

--- js/admin_mug.js
+++ js/admin_mug.js
@@ -296,6 +296,7 @@ var MugShot = {
     name.style.top = parseInt(mug.top) + parseInt(mug.height) + 'px';
     name.style.left = mug.el.style.left;
     name.style.width = mug.el.style.width;
+    name.autocomplete = false;
     document.getElementById(this.id2).append(name);
     this.mugs[this.cfi].name.el = name;
     this.mugs[this.cfi].frame.el.title = name.value;
